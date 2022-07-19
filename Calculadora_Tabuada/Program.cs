8b0560a938963d8f535ddd530509a862a1762e26
Juliana <juh.ngn@gmail.com>
2022-07-19 20:26:15 -0300

    Inlcuindo Projeto Calculadora de Mult

---
+++ Calculadora_Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mallu! Bem vinda a sua calculadora de mutiplicação!");
+            Console.WriteLine("Digite o valor que deseja multiplicar hoje: ");
+               int valor = int.Parse (Console.ReadLine());
+
+            int x = 0;
+
+            while (x <= 20)
+            {
+                int multiplicacao = valor * x;
+                Console.WriteLine($"{valor} x {x} : {multiplicacao}.");
+                x++;
+            }
+
+        }
+    }
+}
